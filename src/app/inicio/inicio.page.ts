00b2d05e474d40cc7aabb74865303b50107943a7
Lenin V <lcvelastegui@gmail.com>
2023-03-05 09:07:25 -0500

    ADMIN4 app funcional

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServiciosService } from '../servicios.service';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 import { Storage } from '@ionic/storage-angular';
+import { Route } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -15,6 +16,45 @@ export class InicioPage implements OnInit {
   public clientes: number = 0;
   public usuarios: number = 0;
   public userLogeado;
+
+  public pages = [
+    {
+        name : 'Clientes',
+        img : 'assets/imgs/clientes.png',
+        color: '#48ddcf',
+        route : 'clientes'
+    },
+    {
+      name : 'Usuarios',
+      img : 'assets/imgs/user.png',
+      color: '#f34845',
+      route : 'usuarios'
+    },
+    {
+        name : 'Productos',
+        img : 'assets/imgs/pedidos.png',
+        color: '#46d2ff',
+        route : 'productos'
+    },
+    {
+        name : 'Pedidos',
+        img : 'assets/imgs/carrito.png',
+        color: '#facc03',
+        route : 'pedidos'
+    },
+    {
+        name : 'Reportes',
+        img : 'assets/imgs/reporte.png',
+        color: '#f34845',
+        route : 'reportes'
+    },
+    {
+        name : 'Salir',
+        img : 'assets/imgs/salir.png',
+        color: '#5b69c2',
+        route : 'salir'
+    }
+]
 
   constructor(
     public servicio: ServiciosService,
@@ -46,4 +86,30 @@ export class InicioPage implements OnInit {
     
   }
 
+  goToPage(val) {
+    if(val == 'clientes') {
+        this.servicio.irA('/clientes');
+    }
+    if(val == 'usuarios') {
+      this.servicio.irA('/usuarios');
+    }
+    if(val == 'productos') {
+        this.servicio.irA('/productos');
+    }
+    if(val == 'pedidos') {
+      this.servicio.irA('/pedidos');
+    }
+    if(val == 'reportes') {
+      this.servicio.irA('/reports');
+    }
+    if(val == 'salir') {
+      this.servicio.irA('/login');
+    }
 }
+
+
+ 
+}
+
+
+
